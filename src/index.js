12da0db7205cdcc881e7e326e5fcebf3049fabda
Santiago Araoz <santiagoaraoz@gmail.com>
2020-11-01 19:37:49 -0300

    act puerto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 // Middlewares
 app.use(express.json());
