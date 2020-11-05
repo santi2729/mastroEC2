41b6fe45949947b055bb75a5dd7622d92a7d2f65
Santiago Araoz <santiagoaraoz@gmail.com>
2020-11-05 00:11:35 -0300

    actualizacion puerto3000

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 // Settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
